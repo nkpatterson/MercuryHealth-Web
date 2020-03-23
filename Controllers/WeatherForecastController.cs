4ae6eee7926e51ee08fbea0cb067b53c5bb5df34
Nick Patterson <nkpatterson@github.com>
2020-03-23 14:17:28 -0700

    Update Controllers/WeatherForecastController.cs

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -14,7 +14,7 @@ namespace AspNetCoreVueStarter.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", PHX Hot
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "PHX Hot"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
