4174aa4798b68a0abcc77b5a65483ca0c7aded7d
Nick <nkpatterson@github.com>
2020-06-29 14:06:36 +0000

    Resetting weather for AB#1865

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -14,7 +14,7 @@ namespace AspNetCoreVueStarter.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "PHX"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
